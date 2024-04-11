cb2b6113de2011417ac7abb42f0551324f4332f0
saif-elhady <saifelhady2004@gmail.com>
2024-04-11 19:25:42 +0000

    pro

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,7 @@
+
+rts.logMe = function (item) {
+  if (exports.logMe.count === undefined) {
+    exports.logMe.count = 0;
+  } else { exports.logMe.count++; }
+  console.log(`${exports.logMe.count}: ${item}`);
+};
