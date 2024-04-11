ccca6a8d255da1e0ae40e32d2147c4f7d496e513
saif-elhady <saifelhady2004@gmail.com>
2024-04-11 17:35:59 +0000

    pro

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,4 +3,4 @@ const { argv } = require('process');
 
 const len = argv.length;
 
-if (len == 2) { console.log('No argument'); } else if (len == 3) { console.log('Argument found'); } else { console.log('Arguments found'); }
+if (len === 2) { console.log('No argument'); } else if (len === 3) { console.log('Argument found'); } else { console.log('Arguments found'); }
